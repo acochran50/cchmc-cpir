7574cea2dd753f7fab98beb215b77df138d1033d
Alex Cochran <acochran50@gmail.com>
2018-02-20 17:49:47 -0500

    split project to new directory; add controlling script

---
+++ matlab/tools/test.m
@@ -0,0 +1,14 @@
+{
+    "processType": "multi-TE",
+    "engineerName": "Alex Cochran",
+    "groupName": "CPIR",
+    "settings": {
+        "numProjections": 29556,
+        "numCutProjections": 0,
+        "numPoints": 101,
+        "numSep": 180,
+        "expirationThreshold": 0.27,
+        "inspiratoinThreshold": 0.15,
+        "echoTimes": [80, 200, 400]
+    }
+}
